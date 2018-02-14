30f4c690abcd51523070530972c3e31b7cc5e546
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-02-15 04:15:30 +0530

    semantic-ui-react and semantic-ui-css support

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,60 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+  watch: true,
 
-    watch: true,
+  target: "electron",
 
-    target: 'electron',
+  entry: "./app/src/entry.js",
 
-    entry: './app/src/entry.js',
+  output: {
+    path: __dirname + "/app/build",
+    publicPath: "build/",
+    filename: "bundle.js"
+  },
 
-    output: {
-        path: __dirname + '/app/build',
-        publicPath: 'build/',
-        filename: 'bundle.js'
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['react']
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    loader: 'css-loader'
-                })
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                query: {
-                    name: '[name].[ext]?[hash]'
-                }
-            }
-        ]
-    },
-
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'bundle.css',
-            disable: false,
-            allChunks: true
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["react"]
         }
-    )
-]
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "postcss-loader"
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        query: {
+          name: "[name].[ext]?[hash]"
+        }
+      }
+    ]
+  },
 
-}
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "bundle.css",
+      disable: false,
+      allChunks: true
+    })
+  ]
+};
