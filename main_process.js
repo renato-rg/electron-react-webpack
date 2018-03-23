e8ea32618040eaa0801a299d5859bf6d5d84714a
Andrey Gurtovoy <iyntx@ya.ru>
2018-03-23 17:43:32 +0300

    remove unnecessary `let` statment

--- main_process.js
+++ main_process.js
@@ -10,7 +10,7 @@ let mainWindow
 
 app.on('ready', () => {
 
-    let mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 800, height: 600})
 
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
